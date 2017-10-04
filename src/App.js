19d99a0e63b76ba209ce4359633f13b6229526a5
Joseph Dalmasso <me@jodalmasso.com>
2017-10-04 15:21:07 -0400

    state practive

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import PropTypes from 'prop-types';
 
-const Todo = ({item}) => {
+const Todo = ({item, toggleComplete, removeToDo}) => {
   return (
    <li>{item.title}
       <input
          type="checkbox"
          id={item.id}
-         checked={item.complete} />
+         checked={item.complete}
+         onChange={toggleComplete} />
       <label htmlFor={item.id}></label>
-      <button>
+      <button onClick={removeToDo}>
          <i className="fa fa-trash"></i>
       </button>
    </li>
@@ -20,7 +21,7 @@ const Todo = ({item}) => {
 const ToDoCount = ({number}) => {
   return (
   <div>
-    {number > 1 || number === 0 ? "Todos" : "Todo"}
+    {number > 1 || number === 0 ? number + " " + "Todos" : "Todo"}
   </div>
   )
 }
@@ -33,24 +34,97 @@ const ClearButton = ({removeCompleted}) => {
   )
 }
 
-// removeCompleted(){
-//   console.log("hi")
-// }
-
 class ToDo extends Component{
+  constructor(){
+    super();
+    this.removeCompleted = this.removeCompleted.bind(this);
+
+    this.state = {
+      todos: [{ id: 0, title: 'Learn React', complete: false }, {id: 1, title: 'Love React', complete: false}],
+      lastId: 0
+    }
+  }
+
+  addToDo = (event) => { //the arrow is binding this to addToDo
+    event.preventDefault();
+    const id = this.state.lastId + 1;
+    
+    if(this.toDoInput.value){
+      let todos = this.state.todos.concat({
+        id,
+        title: this.toDoInput.value,
+        complete: false
+      })
+
+      this.setState({
+        todos,
+        lastId: id
+      })
+      this.toDoInput.value = ""
+    }
+  }
+
+  toggleComplete(item){
+    let todos = this.state.todos.map((todo)=>{
+      if (todo.id === item.id){
+        todo.complete = !todo.complete;
+      }
+      return todo;
+    })
+    this.setState({todos});
+  }
+
+  removeToDo(item){
+    let todos = this.state.todos.filter((todo) => {
+      return todo.id !== item.id
+    })
+    this.setState({todos});
+  }
+
+  removeCompleted({removeCompleted}){
+    let todos = this.state.todos.filter((todo => !todo.complete));
+    this.setState({todos});
+  }
+
+  hasCompleted(){
+    let todos= this.state.todos.filter((todo => todo.complete));
+    if(todos.length > 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  componentDidMount(){
+    this.toDoInput.focus();
+  }
+
   render(){
-    const todos =[
-      { id: 0, title: 'Learn React', complete: false }
-    ];
     return (
       <div className="todo-list">
       <h1>Hello</h1>
+
+      <div className="add-todo">
+        <form name="addTodo" onSubmit={this.addToDo}>
+          <input type="text" ref={(input) => (this.toDoInput = input)} />
+          <span>(press enter to add)</span>
+        </form>
+      </div>
+
         <ul>
-          {todos.map((todo, i) => <Todo item= {todo} key={i} />)}
+          {this.state.todos.map((todo, i) => (
+            <Todo
+            item={todo}
+            key={i}
+            toggleComplete={this.toggleComplete.bind(this, todo)}
+            removeToDo={this.removeToDo.bind(this, todo)}
+            />
+          ))}
         </ul>
         <div className="todo-admin">
-          <ToDoCount number={35} />
-          <ClearButton removeCompleted={"this will be this.removeCompleted"} />
+          <ToDoCount number={this.state.todos.length} />
+          {this.hasCompleted() ? <ClearButton removeCompleted={this.removeCompleted} />: ""}
         </div>
       </div>
     );
